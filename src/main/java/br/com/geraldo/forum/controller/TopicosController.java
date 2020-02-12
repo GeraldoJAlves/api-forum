a76c4283b56295efeca6615f964dddf9ed23e685
JuninhoAlves <junior.alves.2007@gmail.com>
2020-02-12 00:20:53 -0300

    Implementando o uso de cache

--- src/main/java/br/com/geraldo/forum/controller/TopicosController.java
+++ src/main/java/br/com/geraldo/forum/controller/TopicosController.java
@@ -1,13 +1,18 @@
 package br.com.geraldo.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,15 +44,37 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> listar() {
+	@Cacheable("listaDeTopicos")
+	public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC) Pageable paginacao) {
 
-		List<Topico> topicos = topicoRepository.findAll();
+		Page<Topico> topicos;
+		if (nomeCurso == null) {
+
+			topicos = topicoRepository.findAll(paginacao);
+		} else {
+
+			topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+		}
 
 		return TopicoDto.converter(topicos);
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<TopicoDetalheDto> detalhe(@PathVariable Long id) {
+
+		Optional<Topico> topico = topicoRepository.findById(id);
+
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new TopicoDetalheDto(topico.get()));
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> salvar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 
 		Topico topico = form.converter(cursoRepository);
@@ -60,6 +88,7 @@ public class TopicosController {
 
 	@PutMapping("{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 
 		Optional<Topico> optionalTopico = topicoRepository.findById(id);
@@ -72,21 +101,9 @@ public class TopicosController {
 		return ResponseEntity.notFound().build();
 	}
 
-	@GetMapping("/{id}")
-	@Transactional
-	public ResponseEntity<TopicoDetalheDto> detalhe(@PathVariable Long id) {
-
-		Optional<Topico> topico = topicoRepository.findById(id);
-
-		if (topico.isPresent()) {
-			return ResponseEntity.ok(new TopicoDetalheDto(topico.get()));
-		}
-
-		return ResponseEntity.notFound().build();
-	}
-
 	@DeleteMapping("/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> excluir(@PathVariable Long id) {
 
 		Optional<Topico> topico = topicoRepository.findById(id);
--- src/main/java/br/com/geraldo/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/geraldo/forum/repository/TopicoRepository.java
@@ -1,13 +1,13 @@
 package br.com.geraldo.forum.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.geraldo.forum.model.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 	
-	public List<Topico> findByCursoNome(String nomeCurso);
+	public Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 	
 }
